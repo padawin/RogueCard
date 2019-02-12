582d47c6b0c5c6eb0e445b726c636cf6f7e6aa8e
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2019-02-12 10:01:02 +0000

    Single resolution used

--- src/globals.hpp
+++ src/globals.hpp
@@ -3,13 +3,11 @@
 
 #if GCW
 const bool FULL_SCREEN = true;
-const int SCREEN_WIDTH = 320;
-const int SCREEN_HEIGHT = 240;
 #else
 const bool FULL_SCREEN = false;
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
 #endif
+const int SCREEN_WIDTH = 320;
+const int SCREEN_HEIGHT = 240;
 const char * const GAME_DATA_FOLDER = ".card-rogue/";
 
 #endif
