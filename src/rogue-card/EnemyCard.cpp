095274add6181fce1ec9d976ade499602d047bb8
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2019-02-15 21:30:06 +0000

    Enemies meta stored in the EnemyCard class

--- src/rogue-card/EnemyCard.cpp
+++ src/rogue-card/EnemyCard.cpp
@@ -1,8 +1,18 @@
 #include "EnemyCard.hpp"
 
+ResourceManager<S_EnemyMeta> EnemyCard::m_enemyMeta = ResourceManager<S_EnemyMeta>();
+
 EnemyCard::EnemyCard() : Card(EnemyCardType) {
 	m_sImage = "monsters";
 }
 
 void EnemyCard::create() {
+}
+
+bool EnemyCard::prepareMeta(std::string file) {
+	if (!m_enemyMeta.setResourceFile(file)) {
+		return false;
+	}
+	m_enemyMeta.parseBinaryFile();
+	return true;
 }
--- src/rogue-card/EnemyCard.hpp
+++ src/rogue-card/EnemyCard.hpp
@@ -2,11 +2,16 @@
 #define __ENEMY_CARD__
 
 #include "Card.hpp"
+#include "../common/types.hpp"
+#include "../common/ResourceManager.hpp"
 
 class EnemyCard : public Card {
+	static ResourceManager<S_EnemyMeta> m_enemyMeta;
+
 	public:
 	EnemyCard();
 	void create();
+	static bool prepareMeta(std::string file);
 };
 
 #endif
