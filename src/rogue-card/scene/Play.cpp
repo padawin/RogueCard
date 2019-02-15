1236cfce4245ffcc43753c1f12fdb2ee3db2c3dc
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2019-02-15 12:19:43 +0000

    Skeleton of PlayState added to use a selected card

--- src/rogue-card/scene/Play.cpp
+++ src/rogue-card/scene/Play.cpp
@@ -34,6 +34,9 @@ void PlayScene::update(StateMachine &stateMachine) {
 		stateMachine.clean();
 		return;
 	}
+	else if (m_userActions.getActionState("USE_CARD")) {
+		_useCardUnderCursor();
+	}
 }
 
 void PlayScene::render() {
@@ -42,4 +45,73 @@ void PlayScene::render() {
 
 void PlayScene::_renderBackground() const {
 	TextureManager::Instance()->drawImage("ui", m_renderer->getRenderer());
+}
+
+void PlayScene::_useCardUnderCursor() {
+	switch (m_cursorPosition) {
+		case Deck:
+			_pickCard();
+			break;
+		case Action:
+			_action();
+			break;
+		case Object1:
+			_useObject(0);
+			break;
+		case Object2:
+			_useObject(1);
+			break;
+		case Object3:
+			_useObject(2);
+			break;
+		case Object4:
+			_useObject(3);
+			break;
+		case Floor:
+			_changeFloor();
+			break;
+		default:
+			break;
+	}
+}
+
+void PlayScene::_pickCard() {
+	if (m_pickedCard == nullptr) {
+		std::cout << "Pick card\n";
+	}
+	else {
+		_notify("A card is already picked");
+	}
+}
+
+void PlayScene::_action() {
+	if (m_pickedCard != nullptr) {
+		std::cout << "Action\n";
+	}
+	else {
+		_notify("Pick a card first");
+	}
+}
+
+void PlayScene::_useObject(int objectIndex) {
+	if (objectIndex >= MAX_OBJECTS) {
+		return;
+	}
+
+	if (m_objectCards[objectIndex] != nullptr) {
+		std::cout << "Use object\n";
+	}
+}
+
+void PlayScene::_changeFloor() {
+	if (m_floorCard != nullptr) {
+		std::cout << "Change floor\n";
+	}
+	else {
+		_notify("You haven't found the next floor yet");
+	}
+}
+
+void PlayScene::_notify(std::string message) const {
+	std::cout << message << std::endl;
 }
