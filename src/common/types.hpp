c4e30c744e386680427bd863f2f5e7b3f3f12bc0
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2019-02-15 21:06:18 +0000

    First enemy meta added

--- src/common/types.hpp
+++ src/common/types.hpp
@@ -4,9 +4,23 @@
 #define MAX_LENGTH_TILESET_NAME 32
 #define MAX_CHAR_RESOURCE_PATH 255
 
+#define MAX_CHAR_ENEMY_NAME 32
+
 struct S_TilesetMapping {
 	char tileset[MAX_LENGTH_TILESET_NAME];
 	char filePath[MAX_CHAR_RESOURCE_PATH];
+};
+
+struct S_EnemyMeta {
+	char name[MAX_CHAR_ENEMY_NAME];
+	int health;
+	int strength;
+	int defense;
+	int tilesetX;
+	int tilesetY;
+	char dropRate;
+	char minItems;
+	char maxItems;
 };
 
 #endif
