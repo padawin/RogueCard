855c23b631816cf3144e0ef8277a2a65792974e8
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2019-02-17 23:58:08 +0000

    Inventory cursor implemented

--- src/rogue-card/scene/Inventory.cpp
+++ src/rogue-card/scene/Inventory.cpp
@@ -9,6 +9,24 @@ InventoryScene::InventoryScene(UserActions &userActions, Player &player, std::sh
 	m_player(player),
 	m_renderer(renderer)
 {
+	m_mCursorPositions[0] = {16, 16};
+	m_mCursorPositions[1] = {64, 16};
+	m_mCursorPositions[2] = {112, 16};
+	m_mCursorPositions[3] = {160, 16};
+	m_mCursorPositions[4] = {208, 16};
+	m_mCursorPositions[5] = {256, 16};
+	m_mCursorPositions[6] = {16, 80};
+	m_mCursorPositions[7] = {64, 80};
+	m_mCursorPositions[8] = {112, 80};
+	m_mCursorPositions[9] = {160, 80};
+	m_mCursorPositions[10] = {208, 80};
+	m_mCursorPositions[11] = {256, 80};
+	m_mCursorPositions[12] = {16, 144};
+	m_mCursorPositions[13] = {64, 144};
+	m_mCursorPositions[14] = {112, 144};
+	m_mCursorPositions[15] = {160, 144};
+	m_mCursorPositions[16] = {208, 144};
+	m_mCursorPositions[17] = {256, 144};
 }
 
 std::string InventoryScene::getStateID() const {
@@ -23,14 +41,91 @@ void InventoryScene::update(StateMachine &stateMachine) {
 	if (m_userActions.getActionState("BACK")) {
 		stateMachine.popState();
 	}
+	else if (m_userActions.getActionState("CURSOR_UP")) {
+		_moveCursor('N');
+	}
+	else if (m_userActions.getActionState("CURSOR_DOWN")) {
+		_moveCursor('S');
+	}
+	else if (m_userActions.getActionState("CURSOR_LEFT")) {
+		_moveCursor('W');
+	}
+	else if (m_userActions.getActionState("CURSOR_RIGHT")) {
+		_moveCursor('E');
+	}
+}
+
+void InventoryScene::_moveCursor(char direction) {
+	if (direction == 'N') {
+		if (m_cursorPosition < 6) {
+			// go at the bottom
+			if (m_iPage == 1) {
+				m_cursorPosition += 12;
+				m_iPage = 4; // hardcoded to display the 18 last items, @TODO maybe compute?
+			}
+			else {
+				m_iPage--;
+			}
+		}
+		else {
+			m_cursorPosition -= 6;
+		}
+	}
+	else if (direction == 'S') {
+		if (m_cursorPosition >= 12) {
+			// go at the top
+			if (m_iPage == 4) {
+				m_cursorPosition -= 12;
+				m_iPage = 1; // hardcoded to display the 18 first items, @TODO maybe compute?
+			}
+			else {
+				m_iPage++;
+			}
+		}
+		else {
+			m_cursorPosition += 6;
+		}
+	}
+	else if (direction == 'W') {
+		if (m_cursorPosition % 6 == 0) {
+			m_cursorPosition += 5;
+		}
+		else {
+			m_cursorPosition--;
+		}
+	}
+	else if (direction == 'E') {
+		if ((m_cursorPosition + 1) % 6 == 0) {
+			m_cursorPosition -= 5;
+		}
+		else {
+			m_cursorPosition++;
+		}
+	}
 }
 
 void InventoryScene::render() {
 	_renderBackground();
+	_renderCursor();
+	_renderCards();
 }
 
 void InventoryScene::_renderBackground() const {
 	TextureManager::Instance()->drawImage(
 		"ui-inventory", m_renderer->getRenderer(), 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT
 	);
+}
+
+void InventoryScene::_renderCursor() {
+	TextureManager::Instance()->drawImage(
+		"cursor",
+		m_renderer->getRenderer(),
+		m_mCursorPositions[m_cursorPosition].first,
+		m_mCursorPositions[m_cursorPosition].second,
+		48,
+		64
+	);
+}
+
+void InventoryScene::_renderCards() {
 }
--- src/rogue-card/scene/Inventory.hpp
+++ src/rogue-card/scene/Inventory.hpp
@@ -6,12 +6,22 @@
 #include "../sdl2/Renderer.hpp"
 #include "../Player.hpp"
 
+#define INVENTORY_PAGE_SIZE 18
+
 class InventoryScene : public State {
 	private:
 	Player &m_player;
 	std::shared_ptr<SDL2Renderer> m_renderer;
 
+	int m_cursorPosition = 0;
+	std::pair<int, int> m_mCursorPositions[INVENTORY_PAGE_SIZE] = {};
+	int m_iPage = 1;
+
+	void _moveCursor(char direction);
+
 	void _renderBackground() const;
+	void _renderCards();
+	void _renderCursor();
 
 	public:
 	InventoryScene(UserActions &userActions, Player &player, std::shared_ptr<SDL2Renderer> renderer);
