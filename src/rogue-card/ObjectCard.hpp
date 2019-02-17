7370ecf5cde929af997541d0582ae6cec8a10c2f
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2019-02-17 00:00:43 +0000

    Objects loaded randomly from the meta

--- src/rogue-card/ObjectCard.hpp
+++ src/rogue-card/ObjectCard.hpp
@@ -1,12 +1,18 @@
 #ifndef __OBJECT_CARD__
 #define __OBJECT_CARD__
 
+#include "../common/types.hpp"
+#include "../common/ResourceManager.hpp"
 #include "Card.hpp"
 
 class ObjectCard : public Card {
+	static ResourceManager<S_ObjectMeta> m_objectMeta;
+	char m_sName[MAX_CHAR_OBJECT_NAME];
+
 	public:
 	ObjectCard();
 	void create();
+	static bool prepareMeta(std::string file);
 };
 
 #endif
