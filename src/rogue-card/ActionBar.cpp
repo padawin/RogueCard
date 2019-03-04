a8dfe5f6807eb242465ad7a49543fefae9312e18
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2019-03-04 13:00:04 +0000

    The quick action bar uses explicitely ObjectCards

--- src/rogue-card/ActionBar.cpp
+++ src/rogue-card/ActionBar.cpp
@@ -1,6 +1,6 @@
 #include "ActionBar.hpp"
 
-std::shared_ptr<Card> ActionBar::getCard(int index) const {
+std::shared_ptr<ObjectCard> ActionBar::getCard(int index) const {
 	if (index > ACTION_BAR_SIZE) {
 		return nullptr;
 	}
--- src/rogue-card/ActionBar.hpp
+++ src/rogue-card/ActionBar.hpp
@@ -2,16 +2,16 @@
 #define __ACTION_BAR__
 
 #include <memory>
-#include "Card.hpp"
+#include "ObjectCard.hpp"
 
 const int ACTION_BAR_SIZE= 4;
 
 class ActionBar {
 	private:
-	std::shared_ptr<Card> m_objectCards[ACTION_BAR_SIZE] = {nullptr, nullptr, nullptr, nullptr};
+	std::shared_ptr<ObjectCard> m_objectCards[ACTION_BAR_SIZE] = {nullptr, nullptr, nullptr, nullptr};
 
 	public:
-	std::shared_ptr<Card> getCard(int index) const;
+	std::shared_ptr<ObjectCard> getCard(int index) const;
 };
 
 #endif
