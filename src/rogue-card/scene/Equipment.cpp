d7316fa69ef0e966376a7270c400805d42009dc5
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2019-03-08 17:03:07 +0000

    Cursor added

--- src/rogue-card/scene/Equipment.cpp
+++ src/rogue-card/scene/Equipment.cpp
@@ -6,7 +6,16 @@
 EquipmentScene::EquipmentScene(UserActions &userActions, std::shared_ptr<SDL2Renderer> renderer) :
 	State(userActions),
 	m_renderer(renderer)
-{}
+{
+	m_mCursorPositions[0] = {16, 8};
+	m_mCursorPositions[1] = {64, 8};
+	m_mCursorPositions[2] = {112, 8};
+	m_mCursorPositions[3] = {160, 8};
+	m_mCursorPositions[4] = {208, 8};
+	m_mCursorPositions[5] = {256, 8};
+	m_mCursorPositions[6] = {16, 72};
+	m_mCursorPositions[7] = {256, 72};
+}
 
 std::string EquipmentScene::getStateID() const {
 	return "EquipmentScene";
@@ -20,10 +29,17 @@ void EquipmentScene::update(StateMachine &stateMachine) {
 	if (m_userActions.getActionState("BACK")) {
 		stateMachine.popState();
 	}
+	else if (m_userActions.getActionState("CURSOR_LEFT")) {
+		m_cursorPosition = (EQUIPMENT_SIZE + m_cursorPosition - 1) % EQUIPMENT_SIZE;
+	}
+	else if (m_userActions.getActionState("CURSOR_RIGHT")) {
+		m_cursorPosition = (m_cursorPosition + 1) % EQUIPMENT_SIZE;
+	}
 }
 
 void EquipmentScene::render() {
 	_renderBackground();
+	_renderCursor();
 }
 
 void EquipmentScene::_renderBackground() const {
@@ -32,3 +48,13 @@ void EquipmentScene::_renderBackground() const {
 	);
 }
 
+void EquipmentScene::_renderCursor() {
+	TextureManager::Instance()->draw(
+		"cursor",
+		m_mCursorPositions[m_cursorPosition].x,
+		m_mCursorPositions[m_cursorPosition].y,
+		48,
+		64,
+		m_renderer->getRenderer()
+	);
+}
--- src/rogue-card/scene/Equipment.hpp
+++ src/rogue-card/scene/Equipment.hpp
@@ -4,13 +4,19 @@
 #include <memory>
 #include <string.h>
 #include "../game/State.hpp"
+#include "../game/types.hpp"
 #include "../sdl2/Renderer.hpp"
+
+#define EQUIPMENT_SIZE 8
 
 class EquipmentScene : public State {
 	private:
 	std::shared_ptr<SDL2Renderer> m_renderer;
+	int m_cursorPosition = 0;
+	S_Coordinates m_mCursorPositions[EQUIPMENT_SIZE] = {};
 
 	void _renderBackground() const;
+	void _renderCursor();
 
 	public:
 	EquipmentScene(UserActions &userActions, std::shared_ptr<SDL2Renderer> renderer);
