57c429ab9928eb73b71ef07cc9ed285a2cb10ebe
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2019-04-03 19:40:53 +0100

    2 series of ifs merged together

--- src/rogue-card/CardDeck.cpp
+++ src/rogue-card/CardDeck.cpp
@@ -6,31 +6,17 @@
 
 std::shared_ptr<Card> CardDeck::pickCard(Player &player, bool foundNextFloor) {
 	int proba = rand() % 1000;
-	E_CardType type;
-	if (!foundNextFloor && proba < 50) {
-		type = FloorCardType;
-	}
-	else if (proba < 600) {
-		type = EnemyCardType;
-	}
-	else {
-		type = ObjectCardType;
-	}
 	std::shared_ptr<Card> card = nullptr;
-
-	if (type == ObjectCardType) {
-		card = std::shared_ptr<ObjectCard>(new ObjectCard());
-	}
-	else if (type == FloorCardType) {
+	if (!foundNextFloor && proba < 50) {
 		FloorDirection floorDirection = player.getDirection() ? FLOOR_DOWN : FLOOR_UP;
 		card = std::shared_ptr<FloorCard>(new FloorCard(floorDirection));
 	}
-	else if (type == EnemyCardType) {
+	else if (proba < 600) {
 		card = std::shared_ptr<EnemyCard>(new EnemyCard(player.getFloor().getLevel()));
 		player.setFighting(true);
 	}
 	else {
-		std::cerr << "Unknown card type " << type << "!\n";
+		card = std::shared_ptr<ObjectCard>(new ObjectCard());
 	}
 	card->create();
 	return card;
