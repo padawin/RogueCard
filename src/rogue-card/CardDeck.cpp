9b3a4142a8eb919c3678e927f7758934d6fc5080
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2019-04-03 20:12:30 +0100

    Do not pick a floor card on the last level

--- src/rogue-card/CardDeck.cpp
+++ src/rogue-card/CardDeck.cpp
@@ -6,13 +6,14 @@
 #include "FinalGoalCard.hpp"
 
 std::shared_ptr<Card> CardDeck::pickCard(Player &player, bool foundNextFloor) {
+	bool isLastFloor = player.getFloor().isLast();
 	int proba = rand() % 1000;
 	std::shared_ptr<Card> card = nullptr;
-	if (player.getFloor().isLast() && proba < 10) {
+	if (isLastFloor && proba < 10) {
 		card = std::shared_ptr<FinalGoalCard>(new FinalGoalCard());
 		m_bFoundFinalGoal = true;
 	}
-	else if (!m_bFoundFinalGoal && !foundNextFloor && proba < 50) {
+	else if (!m_bFoundFinalGoal && !isLastFloor && !foundNextFloor && proba < 50) {
 		FloorDirection floorDirection = player.getDirection() ? FLOOR_DOWN : FLOOR_UP;
 		card = std::shared_ptr<FloorCard>(new FloorCard(floorDirection));
 	}
