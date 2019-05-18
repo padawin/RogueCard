0e601d5818322aeffe421e739afbaf713261b68e
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2019-05-18 15:23:16 +0100

    Return if the file already exists

--- src/tools/resourceEditor/action/create.go
+++ src/tools/resourceEditor/action/create.go
@@ -1,8 +1,12 @@
 package action
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 var ERR_INVALID_ARGS_LEN = "Invalid numbers of arguments (provided %d, expected %d)"
+var ERR_FILE_EXISTS = "File %s already exists"
 
 func Create(args []string) (int, string) {
 	nb_args := len(args)
@@ -11,6 +15,15 @@ func Create(args []string) (int, string) {
 		msg := fmt.Sprintf(ERR_INVALID_ARGS_LEN, nb_args, expected_nb_args)
 		return 1, msg
 	}
-	fmt.Println("Create file", args[0])
+	filename := args[0]
+	if fileExists(filename) {
+		msg := fmt.Sprintf(ERR_FILE_EXISTS, filename)
+		return 2, msg
+	}
 	return 0, ""
+}
+
+func fileExists(filename string) bool {
+	_, err := os.Stat(filename)
+	return err == nil
 }
