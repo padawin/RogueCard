007d68e539e32d6087cf61953557483bb49bb353
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2019-05-18 15:22:24 +0100

    Template moved in separate var

--- src/tools/resourceEditor/action/create.go
+++ src/tools/resourceEditor/action/create.go
@@ -2,11 +2,13 @@ package action
 
 import "fmt"
 
+var ERR_INVALID_ARGS_LEN = "Invalid numbers of arguments (provided %d, expected %d)"
+
 func Create(args []string) (int, string) {
 	nb_args := len(args)
 	expected_nb_args := 1
 	if nb_args != expected_nb_args {
-		msg := fmt.Sprintf("Invalid numbers of arguments (provided %d, expected %d)", nb_args, expected_nb_args)
+		msg := fmt.Sprintf(ERR_INVALID_ARGS_LEN, nb_args, expected_nb_args)
 		return 1, msg
 	}
 	fmt.Println("Create file", args[0])
