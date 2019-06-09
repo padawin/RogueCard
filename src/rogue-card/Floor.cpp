4cd56f3e02595b28b58834eea0b617014ec85a02
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2019-06-09 17:22:39 +0100

    floor content meta parsed and stored statically in the Floor class

--- src/rogue-card/Floor.cpp
+++ src/rogue-card/Floor.cpp
@@ -1,6 +1,28 @@
 #include "Floor.hpp"
+#include "../common/ResourceManager.hpp"
 
 const int MAX_LEVEL = 16;
+
+std::map<int, std::vector<S_FloorContent>> Floor::m_mFloorContentMeta = {};
+
+bool Floor::prepareMeta(std::string file) {
+	ResourceManager<S_FloorContent> meta = ResourceManager<S_FloorContent>();
+	if (!meta.setResourceFile(file)) {
+		return false;
+	}
+	meta.parseBinaryFile();
+	auto data = meta.getParsedResources();
+	int nbRows = (int) data.size();
+	int floorLevel;
+	for (int i = 0; i < nbRows; ++i) {
+		floorLevel = data[i].floorLevel;
+		if (m_mFloorContentMeta.find(floorLevel) == m_mFloorContentMeta.end()) {
+			m_mFloorContentMeta[floorLevel] = std::vector<S_FloorContent>();
+		}
+		m_mFloorContentMeta[floorLevel].push_back(data[i]);
+	}
+	return true;
+}
 
 int Floor::getLevel() const {
 	return m_iLevel;
--- src/rogue-card/Floor.hpp
+++ src/rogue-card/Floor.hpp
@@ -1,13 +1,19 @@
 #ifndef __FLOOR__
 #define __FLOOR__
 
+#include <map>
+#include <vector>
+#include "../common/types.hpp"
+
 enum FloorDirection {FLOOR_DOWN, FLOOR_UP};
 
 class Floor {
 	private:
+	static std::map<int, std::vector<S_FloorContent>> m_mFloorContentMeta;
 	int m_iLevel = 1;
 
 	public:
+	static bool prepareMeta(std::string file);
 	int getLevel() const;
 	void setLevel(int level);
 	void toNextLevel();
