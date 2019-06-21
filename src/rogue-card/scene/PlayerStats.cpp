c2d3dbecaf5c560217a8be40bd382bb3c52faa9c
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2019-06-21 16:26:30 +0100

    Cursor added

--- src/rogue-card/scene/PlayerStats.cpp
+++ src/rogue-card/scene/PlayerStats.cpp
@@ -3,10 +3,16 @@
 #include "../game/StateMachine.hpp"
 #include "../sdl2/TextureManager.hpp"
 
+const int STAT_CURSOR_WIDTH = 144;
+const int STAT_CURSOR_HEIGHT = 14;
+
 PlayerStatsScene::PlayerStatsScene(UserActions &userActions, std::shared_ptr<SDL2Renderer> renderer) :
 	State(userActions),
 	m_renderer(renderer)
-{}
+{
+	m_mCursorPositions[Stats] = {9, 8};
+	m_mCursorPositions[Levels] = {167, 8};
+}
 
 std::string PlayerStatsScene::getStateID() const {
 	return "PlayerStatsScene";
@@ -20,14 +26,32 @@ void PlayerStatsScene::update(StateMachine &stateMachine) {
 	if (m_userActions.getActionState("BACK")) {
 		stateMachine.popState();
 	}
+	else if (m_userActions.getActionState("CURSOR_LEFT")) {
+		m_cursorPosition = (StatCursorPosition) ((NbStatPositions + m_cursorPosition - 1) % NbStatPositions);
+	}
+	else if (m_userActions.getActionState("CURSOR_RIGHT")) {
+		m_cursorPosition = (StatCursorPosition) ((m_cursorPosition + 1) % NbStatPositions);
+	}
 }
 
 void PlayerStatsScene::render() {
 	_renderBackground();
+	_renderCursor();
 }
 
 void PlayerStatsScene::_renderBackground() const {
 	TextureManager::Instance()->draw(
 		"ui-player-stats", 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, m_renderer->getRenderer()
+	);
+}
+
+void PlayerStatsScene::_renderCursor() {
+	TextureManager::Instance()->draw(
+		"cursor-player-stats",
+		m_mCursorPositions[m_cursorPosition].x,
+		m_mCursorPositions[m_cursorPosition].y,
+		STAT_CURSOR_WIDTH,
+		STAT_CURSOR_HEIGHT,
+		m_renderer->getRenderer()
 	);
 }
--- src/rogue-card/scene/PlayerStats.hpp
+++ src/rogue-card/scene/PlayerStats.hpp
@@ -3,14 +3,20 @@
 
 #include <string.h>
 #include <memory>
+#include "../game/types.hpp"
 #include "../game/State.hpp"
 #include "../sdl2/Renderer.hpp"
+
+enum StatCursorPosition {Stats, Levels, NbStatPositions};
 
 class PlayerStatsScene : public State {
 	private:
 	std::shared_ptr<SDL2Renderer> m_renderer;
+	StatCursorPosition m_cursorPosition = Stats;
+	std::map<StatCursorPosition, S_Coordinates> m_mCursorPositions = {};
 
 	void _renderBackground() const;
+	void _renderCursor();
 
 	public:
 	PlayerStatsScene(UserActions &userActions, std::shared_ptr<SDL2Renderer> renderer);
