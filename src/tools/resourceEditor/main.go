0974e706496c939f68033d34196d33887c4632b0
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2019-07-23 17:09:51 +0200

    Error handling added

--- src/tools/resourceEditor/main.go
+++ src/tools/resourceEditor/main.go
@@ -1,9 +1,15 @@
 package main
 
+import "fmt"
 import "os"
 import "./action"
 
 func main() {
+	if len(os.Args) == 1 {
+		fmt.Println("Action missing")
+		return
+	}
+
 	actionName := os.Args[1]
 	// @XXX How to know where is `Run` defined?
 	action.Run(actionName, os.Args[2:])
