a9e2f016a2a1515cc6ee77914958fdc5428394be
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2019-09-07 15:14:43 +0200

    Id of FloorCard and FinalGoalCard stored in const

--- src/rogue-card/FinalGoalCard.hpp
+++ src/rogue-card/FinalGoalCard.hpp
@@ -3,12 +3,14 @@
 
 #include "Card.hpp"
 
+const char FINAL_GOAL_CARD_ID[MAX_CHAR_CARD_ID] = "FINAL_GOAL";
+
 class FinalGoalCard : public Card {
 	public:
 	FinalGoalCard();
 	void create();
 	const char* getName() const { return ""; }
-	const char* getID() const { return "FINAL_GOAL"; }
+	const char* getID() const { return FINAL_GOAL_CARD_ID; }
 };
 
 #endif
